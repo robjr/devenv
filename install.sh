9f1406fa7283013abfc078af296c66e1847aed4b
Roberto Júnior <roberto@propertyguru.com.sg>
2020-02-11 12:24:33 +0800

    Install command line tools

--- install.sh
+++ install.sh
@@ -1,6 +1,20 @@
 #!/usr/bin/env bash
+function install() {
+  if ! type "$1" &> /dev/null
+  then
+    eval "$2"
+  else
+    echo "$1 already installed"
+  fi
+}
+
 echo "source $(pwd)/bashProfile.sh" >> ~/.bashrc
 echo "source $(pwd)/bashProfile.sh" >> ~/.profile
 
 rm -f ~/.gitconfig
 ln -s $(pwd)/gitconfig ~/.gitconfig
+
+install "k9s" "brew install derailed/k9s/k9s && curl https://raw.githubusercontent.com/derailed/k9s/master/skins/dracula.yml > $HOME/.k9s/skin.yml"
+install "fzf" "brew install fzf"
+install "kubectx" "brew install kubectx"
+install "ag" "brew install the_silver_searcher"
